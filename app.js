e36118416e00265958057f1c14f8a51cc28a7cfd
alfredo <alfredo.supanta.26@gmail.com>
2022-01-27 23:08:18 -0300

    Se añade boton limpiar al formulario, formulario terminado

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function validacion(){
 
     //el for recorre los inputs radios
     for(let i = 0;i < radios.length;i++){
-        console.log(radios[i])//devuelve ambos nodos de input 
+        //console.log(radios[i])//devuelve ambos nodos de input 
         if(radios[i].checked){//checked es si el input esta seleccionado
             valorRadio = radios[i].value;//guarda en la var el valor checkeado
             //console.log(valorRadio)
@@ -44,6 +44,20 @@ function validacion(){
         console.log(`Nombre: ${nombre} \nSabe programar: ${valorRadio} \nEs estudiante:  ${estudia}`);//interpolacion de datos
 }
 
+function limpiar(){
+    document.getElementById("nombre").value = "";
+    let radios = document.getElementsByName("programar");
+
+    for(let i = 0; i < radios.length; i++){
+        if(radios[i].checked){//recordar que al agregar [] a radios, se pintara el elemento que tenga la posicion [n], al agregar el i, el bucle por cada elemento dentro del array devolvera la posicino ej:radios[0], radios[1], radios[2], etc
+            radios[i].checked = false;// esta linea devolvera cada elemento y al que este checked true lo dejara en false.
+        }
+    }
+    
+    let estudia = document.getElementById("estudiante");
+    estudia.checked = false;
+
+}
 
 
     /* for (let i = 0; i < radios.length; i++){
